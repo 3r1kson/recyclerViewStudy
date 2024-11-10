751ec910e0b325afa01f14d000c4b6eda65c5dfc
Erikson <eriksonribeiro84@gmail.com>
2024-11-10 16:46:46 -0300

    full study finished

---
+++ app/src/main/java/com/example/viewbindingrv/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.viewbindingrv
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.viewbindingrv.TaskList.taskList
+import com.example.viewbindingrv.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val adapter = MainAdapter(TaskList.taskList)
+        binding.taskRv.adapter = adapter
+    }
+}
